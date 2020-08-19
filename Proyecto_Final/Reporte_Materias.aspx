18713f2f06b82a43d8dd9e5b876a83a9ed705cc7
JESSICA DANIELA <jesdanis2103@gmail.com>
2020-08-19 09:26:19 -0500

    Reporte_Materias

---
+++ Proyecto_Final/Reporte_Materias.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Pagina_Principal.Master" AutoEventWireup="true" CodeBehind="Reporte_Materias.aspx.cs" Inherits="Proyecto_Final.Formulario_web1" %>
+
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <h1 style="margin-top: 7px; margin-bottom: 7px;">Reporte Materias</h1>
+    <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+     <rsweb:ReportViewer ID="ReportViewerMaterias" runat="server" Height="448px" 
+        Width="709px"></rsweb:ReportViewer>
+</asp:Content>
---
+++ Proyecto_Final/Reporte_Materias.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Reporting.WebForms;
+using Proyecto_Final.DatosReportes;
+
+namespace Proyecto_Final
+{
+    public partial class Formulario_web1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                cargarReporteGeneral();
+            }
+        }
+        private void cargarReporteGeneral()
+        {
+            string ruta = "ReporteMaterias.rdlc";
+            ConjuntoDatosDataSet.MateriasDataTable vista = new ConjuntoDatosDataSet.MateriasDataTable();
+            DatosReportes.ConjuntoDatosDataSetTableAdapters.MateriasTableAdapter datos = new DatosReportes.ConjuntoDatosDataSetTableAdapters.MateriasTableAdapter();
+            ReportDataSource rs = new ReportDataSource();
+            datos.Fill(vista);
+            rs.Name = "DataSetMaterias";
+            rs.Value = vista;
+            ReportViewerMaterias.LocalReport.DataSources.Clear();
+            ReportViewerMaterias.LocalReport.DataSources.Add(rs);
+            ReportViewerMaterias.ProcessingMode = ProcessingMode.Local;
+            ReportViewerMaterias.LocalReport.ReportPath = ruta;
+            ReportViewerMaterias.LocalReport.Refresh();
+        }
+    }
+}
